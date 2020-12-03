7d2b12c66d2f68f179bb74423e10e6046c3142a8
Thiago Coutinho <thcoutinho1997@gmail.com>
2020-12-03 10:40:06 -0300

    Testes CRUD com constante

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,13 +2,44 @@ const express = require('express');
 
 const server = express();
 
-const cursos = ['Node JS', 'Javascript', 'React Native'];
+server.use(express.json());
 
-server.get('/curso/:index', (req, res) =>{
+const cursos = ['Node JS', 'Javascript', 'Delphi'];
+
+server.get('/cursos', (req, res) =>{
+  return res.json(cursos);
+})
+
+server.get('/cursos/:index', (req, res) =>{
   
   const { index } = req.params;
   
   return res.json(cursos[index]); 
 });
+
+//POST
+server.post('/cursos', (req, res) =>{
+  const {name} = req.body;
+  cursos.push(name);
+
+  return res.json(cursos);
+})
+
+//PUT
+server.put('/cursos/:index', (req, res) =>{
+  const { index } = req.params;
+  const { name } = req.body;
+
+  cursos [index] = name;
+  return res.json(cursos);
+});
+
+//DELETE
+server.delete('/cursos/:index', (req, res) => {
+  const { index } = req.params;
+
+  cursos.splice(index, 1);
+  return res.json({message: 'Curso deletado com Sucesso'});
+})
 
 server.listen(3000);
